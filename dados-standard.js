90e352626f1b541e9bcf9131125b5949b04f85d2
LucasPaschoa <lucas.paschoa@advicegrowth.com.br>
2025-05-29 14:13:09 -0300

    Add files via upload

--- dados-standard.js
+++ dados-standard.js
@@ -1,1 +1,23 @@
-const tabelaStandard = [{ sigla_origem: "SP", sigla_destino: "RJ", minimo: 80, adicional: 3 }];
+
+const tabelaStandard = [
+  {
+    sigla_origem: "SP01",
+    cidade_origem: "São Paulo",
+    uf_origem: "SP",
+    sigla_destino: "RJ01",
+    cidade_destino: "Rio de Janeiro",
+    uf_destino: "RJ",
+    minimo: 50,
+    adicional: 2.5
+  },
+  {
+    sigla_origem: "MG01",
+    cidade_origem: "Belo Horizonte",
+    uf_origem: "MG",
+    sigla_destino: "BA01",
+    cidade_destino: "Salvador",
+    uf_destino: "BA",
+    minimo: 60,
+    adicional: 3.0
+  }
+];
